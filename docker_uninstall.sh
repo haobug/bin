8de1eb95741cbfdf29097933b1f9f4d6bfeb3dab
haobug <qingxianhao@gmail.com>
2021-03-02 13:12:40 +0800

    Update docker_uninstall.sh

--- docker_uninstall.sh
+++ docker_uninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+sudo systemctl disable docker.service
+sudo systemctl disable containerd.service
 dpkg -l | grep -i docker && \
-sudo apt-get purge -y docker-engine docker docker.io docker-ce  && \
+sudo apt-get purge docker-ce docker-ce-cli containerd.io
+sudo apt-get purge docker-engine docker docker.io docker-ce
 sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce  && \
 sudo service docker stop 
 sudo rm -rf /var/lib/docker
