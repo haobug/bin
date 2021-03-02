5a438ce267835d56759c868bb9ba63cc81de2ff7
haobug <qingxianhao@gmail.com>
2021-03-02 13:13:12 +0800

    Update docker.install.sh (#3)
    * Update docker.install.sh
    
    * Update docker_uninstall.sh

--- docker.install.sh
+++ docker.install.sh
@@ -1,15 +1,13 @@
 #sudo apt-get remove docker docker-engine docker.io
 #docker_uninstall.sh
 sudo apt-get remove docker docker-engine docker.io containerd runc && \
-sudo apt-get update && \
-
-sudo apt-get install \
+sudo apt-get update
+sudo apt-get install -y \
 		 apt-transport-https \
 		 ca-certificates \
 		 curl \
 		 gnupg-agent \
 		 software-properties-common && \
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
 
 sudo apt-key fingerprint 0EBFCD88|grep '9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88' && \
@@ -19,11 +17,18 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable" && \
 
-sudo apt-get update && \
+sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io && \
 
 sudo groupadd docker
 sudo usermod -a -G docker $USER
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
 
-echo "docker install Done."
+newgrp docker 
+docker run hello-world
+
+#install as service
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
