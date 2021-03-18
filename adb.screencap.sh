e4ca9bb20eaceef730d99bf1dba91e3830a0ec3d
haobug <qingxianhao@gmail.com>
2021-03-18 22:52:04 +0800

    Update adb.screencap.sh

--- adb.screencap.sh
+++ adb.screencap.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+now()
+{
+date +"%Y%m%d%H%M%S"
+}
 
+pullback()
+{
+adb shell content insert --uri content://settings/system --bind name:s:show_touches --bind value:i:0 < /dev/null
+adb pull /sdcard/$outfile
+xdg-open $outfile || mplayer $outfile
+exit 0
+}
+
+trap 'echo interrupt' INT
+
+ts=`now`
+outfile=$ts.mp4
+adb shell content insert --uri content://settings/system --bind name:s:show_touches --bind value:i:1 < /dev/null
+adb shell screenrecord $@ /sdcard/$outfile < /dev/null
+pullback
